7e9361e371c7bfc2fcf27da059ab05847753584b
贺双双 <hejinyo@gmail.com>
2020-12-30 22:10:45 +0800

    abandon

---
+++ src/test/java/cn/hejinyo/abandon/AbandonApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hejinyo.abandon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbandonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
